65e8787612bcad9b6ef0250294f9798c2d2172ad
shalini-2305 <shalinimunusamy232003@gmail.com>
2025-07-24 13:22:25 +0530

    dev: created section2 and connected, works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@
 
 import Hero from "./lib/components/Hero/hero";
 import Navbar from "./lib/components/Navbar/navbar";
+import Section2 from "./lib/components/Section2/section2";
+import Section3 from "./lib/components/Section3/section3";
 
 export default function Home() {
   return (
     <div className="min-h-screen relative overflow-hidden bg-[#18181b]">
       <Navbar />
       <Hero/>
+      <Section2/>
+      <Section3/>
     </div>
   );
 }
